e712a2590a2d2e03a4a4e1accbe7e04ed397ba6e
Dilip Kumar Raushan <131870489+DilipKumarRaushan@users.noreply.github.com>
2024-02-28 11:15:40 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,28 @@
+const name = "Dilip"
+
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+//console.log(`Hello My Name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("dilip-hc")
+//console.log(gameName[0])
+//console.log(gameName.__proto__);
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf("i"));
+
+
+const newString = gameName.substring(0, 4)
+//console.log(newString)
+
+const anotherString = gameName.slice(-4,1)
+//console.log(anotherString)
+
+const newStringOne = "  dilip   "
+//console.log(newStringOne)
+//console.log(newStringOne.trim());
+
+const url = "https://dilip.com/dilip20kumar"
+//console.log(url.replace('20','-'))
+
+//console.log(gameName.split("-"))
