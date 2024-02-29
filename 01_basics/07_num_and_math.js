b076568e3ceed3ca93f722c3ba6dcf8b1bfd73b0
Dilip Kumar Raushan <131870489+DilipKumarRaushan@users.noreply.github.com>
2024-02-29 04:20:55 +0000

    Date and Time

--- 01_basics/07_num_and_math.js
+++ 01_basics/07_num_and_math.js
@@ -22,9 +22,9 @@ const hundreds = 1000000
 // console.log(Math.floor(4.9))
 // console.log(Math.min(4,3,5,6,7,2,3))
 
-console.log(Math.floor(Math.random()*10)+1)
+//console.log(Math.floor(Math.random()*10)+1)
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random()*(max-min+1))+min)
+//console.log(Math.floor(Math.random()*(max-min+1))+min)
