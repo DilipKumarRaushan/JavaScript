1cdd12f8f23890c7b63c4b6e7bc2912279db75b7
Dilip Kumar Raushan <131870489+DilipKumarRaushan@users.noreply.github.com>
2024-03-02 16:05:22 +0000

    arrow

---
+++ 03_basics/01_arrowfunction.js
@@ -0,0 +1,53 @@
+const user = {
+    username: "dilip",
+    price : 999,
+    welcomeMessage: function(){
+      console.log(`${this.username} welcome to website`)
+      console.log(this)
+    }
+}
+// user.welcomeMessage()
+// user.username = "Raushan"
+// user.welcomeMessage()
+//console.log(this);
+
+// function chai(){
+//     let username = "dilip"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "dilip"
+//     console.log(this.username)
+// }
+// chai()
+
+// const chai = ()=>{
+//     let username="dilip"
+//     console.log(this.username)
+// }
+// chai()
+
+//+++++++++++pure arrow function+++++++++++++++++
+
+// const addTwo = (num1,num2)=>{
+//     return num1 + num2
+// }
+// console.log(addTwo(8,9))
+
+// const addTwo = (num1,num2)=>num1 + num2
+// console.log(addTwo(17,13))
+
+const addTwo = (name)=>
+
+// const addTwo = (num1,num2)=>(num1 + num2)
+// console.log(addTwo(3,4))
+
+// const addTwo = (num1,num2)=>({username:"dilip Kumar"})
+// console.log(addTwo(8,6))
+
+// const addOne = (username,age,roll)=>({username:"dilip",age:30,roll:8})
+// console.log(addOne());
+
+
