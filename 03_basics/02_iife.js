e527c313b839df16da7461da6f124e7d8b26a9c0
Dilip Kumar Raushan <131870489+DilipKumarRaushan@users.noreply.github.com>
2024-03-04 15:09:08 +0000

    iife

---
+++ 03_basics/02_iife.js
@@ -0,0 +1,11 @@
+//Immediately Invoked Function Expression (IIFE)
+
+//named IIFE
+// (function chai(){
+//     console.log("DB CONNECTED");
+// })();
+
+// Un-named IIFE
+((name)=>{
+ console.log(`DB CONNECTED TWO ${name}`);
+})("Dilip Kumar Raushan");
