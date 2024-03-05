7fcfc57de496964b28abf1d9cff0024d1bf511a5
Dilip Kumar Raushan <131870489+DilipKumarRaushan@users.noreply.github.com>
2024-03-05 05:26:01 +0000

    if condition

---
+++ 04_controllflow.js/one.js
@@ -0,0 +1,51 @@
+// if statement
+
+const isUserloggedIn = true
+const temperature = 41
+
+
+// if(temperature === 51){
+//   console.log("less than 50")
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute")
+
+// <, >, <=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User Power : ${power}`)
+// }
+// console.log(`User Power : ${power}`)
+
+
+
+//const balance = 200
+//if(balance > 500) console.log("test");
+
+// const balance = 200
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if(balance < 750){
+//     console.log("less than 750")
+// }else if(balance < 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1250")
+// }
+
+// const userLoggedIn = true 
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==2) {
+//     console.log("Allow to buy course");
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User Logged In")
+// }
