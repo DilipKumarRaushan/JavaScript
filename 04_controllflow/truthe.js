b4bc8a53127bdd0949a6269329479de518693a18
Dilip Kumar Raushan <131870489+DilipKumarRaushan@users.noreply.github.com>
2024-03-05 09:36:52 +0000

    switch

--- 04_controllflow/truthe.js
+++ 04_controllflow/truthe.js
@@ -1,10 +1,11 @@
-const userEmail = "h@dilip.ai"
+// const userEmail = "h@dilip.ai"
 
-if (userEmail) {
-    console.log("Got user email");
-}else{
-    console.log("Don't have user email");
-}
+// if (userEmail) {
+//     console.log("Got user email");
+// }else{
+//     console.log("Don't have user email");
+// }
+
 
 // falsy values
 // false, 0, -0, bigInt 0n, "", null, Undefined, NaN.
@@ -16,10 +17,14 @@ if (userEmail) {
 //     console.log("Array is empty");
 // }
 
-const emptyObj = {}
-if(Object.keys(emptyObj).length === 0){
-    console.log("Object is empty")
-}
+
+
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty")
+// }
+
+
 
 // Nullish Coalescing Operator (??): null undefined
 
@@ -30,9 +35,12 @@ if(Object.keys(emptyObj).length === 0){
 //val1 = null ?? 10 ?? 15
 //console.log(val1);
 
+
+
 //Terniary Operator
 
 //condition ? true : false
 
-const iceTeaPrice = 100
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");;
+// const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
